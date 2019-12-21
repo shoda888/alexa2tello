657e655255d3e6c8703569d077b7eaed3aaf347a
shoda888 <gekka888@gmail.com>
2019-12-21 17:36:04 +0900

    fix docker run command

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-docker run --rm -v "$PWD":/var/task aws-lambda-python3.7-test:latest
+docker run --rm -v "$PWD":/var/task shoda888/local2pylambda:latest
 aws lambda update-function-code --function-name alexa-to-tellopython --zip-file fileb://deploy_package.zip
